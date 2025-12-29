24d8bae8ae9db885d1723df9c36091ab06cceda6
mohim <mmgavad8585@gmail.com>
2025-12-29 23:53:22 +0330

    changeing img of WOCS

--- src/Pages/Home/WOCSSection/WOCSSection.jsx
+++ src/Pages/Home/WOCSSection/WOCSSection.jsx
@@ -16,7 +16,7 @@ function SContent() {
                 <h5 className="text-primary-content text-3xl">What Our Customers Say</h5>
                 <p className="text-center">Integer ut rutrum nulla, sit amet fringilla nisi. Nam nisl velit, vehicula at nibh sed, porttitor sollicitudin ante. Sed justo augue, vestibulum ut efficitur vitae, euismod et justo. Mauris sed mattis ante. Ut ligula lectus, consequat vitae neque ac, gravida hendrerit ante.</p>
                 <div className=" flex flex-col gap-5 items-center">
-                    <img className="rounded-full w-44 h-44 object-cover" src="rot.jpg" alt="" />
+                    <img className="rounded-full w-44 h-44 object-cover" src="telativ.jpg" alt="" />
                     <span className="text-primary-content text-2xl">MOHAMMAD JAVAD</span>
                 </div>
             </div>
