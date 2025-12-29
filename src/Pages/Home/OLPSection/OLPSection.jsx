8349812a264de79a677cb434f308740eaa1ddbd7
mohim <mmgavad8585@gmail.com>
2025-12-29 23:44:57 +0330

    rm ./ from pathes

--- src/Pages/Home/OLPSection/OLPSection.jsx
+++ src/Pages/Home/OLPSection/OLPSection.jsx
@@ -2,7 +2,7 @@ export default function OLPSection() {
     return (
         <>
             <div className="flex flex-col bg-white md:px-15">
-                <img src="./our last project.jpg" />
+                <img src="our last project.jpg" />
             </div>
             <div className="flex flex-col gap-5 items-center py-10 bg-black">
                 <h2 className="text-3xl text-primary-content">Our Latest Projects</h2>
--- src/Pages/Home/OMCSection/OMCSection.jsx
+++ src/Pages/Home/OMCSection/OMCSection.jsx
@@ -6,15 +6,15 @@ export default function OMCSection() {
         <div className='bg-white h-52 w-screen py-10 flex flex-col gap-6 items-center '>
             <h5 className='text-3xl'>Our Main Clients</h5>
             <Swiper className='w-full' modules={[Autoplay]} slidesPerView={2} breakpoints={{500:{slidesPerView:4}}}  spaceBetween={50} autoplay={{delay:2000}}>
-                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="./colorlib-logo.png" alt="" /></SwiperSlide>
-                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="./js-logo.png" alt="" /></SwiperSlide>
-                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="./html5-logo.png" alt="" /></SwiperSlide>
-                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="./css-logo.png" alt="" /></SwiperSlide>
-                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="./less-logo.png" alt="" /></SwiperSlide>
-                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="./sass-logo.png" alt="" /></SwiperSlide>
-                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="./bootstrap-logo.jpg" alt="" /></SwiperSlide>
-                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="./adobe-logo.png" alt="" /></SwiperSlide>
-                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="./facebook-logo.png" alt="" /></SwiperSlide>
+                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="colorlib-logo.png" alt="" /></SwiperSlide>
+                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="js-logo.png" alt="" /></SwiperSlide>
+                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="html5-logo.png" alt="" /></SwiperSlide>
+                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="css-logo.png" alt="" /></SwiperSlide>
+                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="less-logo.png" alt="" /></SwiperSlide>
+                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="sass-logo.png" alt="" /></SwiperSlide>
+                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="bootstrap-logo.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="adobe-logo.png" alt="" /></SwiperSlide>
+                <SwiperSlide className='flex justify-center'><img className='w-12 h-12 mx-auto' src="facebook-logo.png" alt="" /></SwiperSlide>
             </Swiper>
         </div>
     )
